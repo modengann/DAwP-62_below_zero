3d0be5cb7f626f9628f67f336200eed5eb82a994
modengann <99417989+modengann@users.noreply.github.com>
2022-09-22 09:56:43 -0400

    init

---
+++ src/below_zero.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def below_zero():
+    pass
+
+def main():
+    result = below_zero()
+    print(f"Number of days below zero: {result}")
+if __name__ == "__main__":
+    main()
